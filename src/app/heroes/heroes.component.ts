fa7adfec7f52795bc9351a71884a513016000b5b
Zac Woll <zacwoll@gmail.com>
2023-03-28 15:44:49 -0500

    Added a couple of NG directives and hero models

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Hero } from '../Hero';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';;
 
 @Component({
   selector: 'app-heroes',
@@ -11,4 +12,10 @@ export class HeroesComponent {
     id: 1,
     name: 'Windstorm'
   }
+    heroes = HEROES;
+
+    selectedHero?: Hero;
+    onSelect(hero: Hero): void {
+      this.selectedHero = hero;
+    }
 };
