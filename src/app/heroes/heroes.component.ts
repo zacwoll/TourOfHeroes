c5971e207f7dd33a391956c770249ecb080adec9
Zac Woll <zacwoll@gmail.com>
2023-03-28 19:07:56 -0500

    Added the hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';;
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +8,23 @@ import { HEROES } from '../mock-heroes';;
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
-    heroes = HEROES;
+
+  constructor(private heroService: HeroService) {};
+
+    heroes: Hero[] = [];
 
     selectedHero?: Hero;
+
+    ngOnInit(): void {
+      this.getHeroes();
+    }
+
     onSelect(hero: Hero): void {
       this.selectedHero = hero;
+    }
+
+    getHeroes() : void {
+      this.heroes = this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
     }
 };
